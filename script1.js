dc65a603f6b73d6ba2639620c164e4885b5d3b25
TriumfAjvazi <triumfajvazi11@gmail.com>
2024-11-30 19:57:30 +0100

    js1

--- script1.js
+++ script1.js
@@ -1,57 +1,54 @@
 document.getElementById('signupForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
-  
-  
-    if (name === '') {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  if (name === '') {
       alert('Name is required');
       return;
-    }
-  
-    if (email === '' || !validateEmail(email)) {
+  }
+
+  if (email === '' || !validateEmail(email)) {
       alert('Valid email is required');
       return;
-    }
-  
-    if (password === '' || password.length < 6) {
-      alert('Password must be at least 6 characters long');
-      return;
-    }
-  
-    alert('Form submitted successfully!');
-  });
-  
- 
-  function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
   }
+
+  if (!validatePassword(password)) {
+      alert('Password must be at least 8 characters long, include at least one uppercase letter, and contain at least one number.');
+      return;
+  }
+
+  alert('Form submitted successfully!');
+});
+
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+
+function validatePassword(password) {
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+  return passwordRegex.test(password);
+}
 
 
 const passwordInput = document.getElementById('password');
 const togglePasswordButton = document.getElementById('togglePassword');
 const eyeIcon = togglePasswordButton.querySelector('.eye-icon');
 
-
 togglePasswordButton.addEventListener('click', () => {
-
   const isPassword = passwordInput.type === 'password';
-
- 
   passwordInput.type = isPassword ? 'text' : 'password';
 
-
   if (isPassword) {
-    eyeIcon.classList.remove('closed-eye');
-    eyeIcon.classList.add('open-eye');
+      eyeIcon.classList.remove('closed-eye');
+      eyeIcon.classList.add('open-eye');
   } else {
-    eyeIcon.classList.remove('open-eye');
-    eyeIcon.classList.add('closed-eye');
+      eyeIcon.classList.remove('open-eye');
+      eyeIcon.classList.add('closed-eye');
   }
 });
-
-
-  
