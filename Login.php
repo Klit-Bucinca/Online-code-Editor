0687331c261b31ea892bc77a8682a201adf177a1
Klit-Bucinca <klitbucinca@gmail.com>
2025-02-01 14:00:37 +0100

    .

--- Login.php
+++ Login.php
@@ -5,7 +5,7 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conn = new mysqli('localhost', 'root', '', 'user_database');
+$conn = new mysqli('localhost', 'root', '', 'code_editor_db');
 
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
